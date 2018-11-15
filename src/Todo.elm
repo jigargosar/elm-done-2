815e4b7020d2373c6e279b20931cfcd00fdc5c52
Jigar Gosar <jigar.gosar@gmail.com>
2018-11-15 17:28:35 +0530

    todo model store

--- src/Todo.elm
+++ src/Todo.elm
@@ -1,11 +1,13 @@
-module Todo exposing (Todo)
+module Todo exposing (Model)
 
 import Dict exposing (Dict)
+import Json.Decode as D
+import Json.Encode as E exposing (Value)
 import TimeX exposing (Millis)
 
 
-type Todo
-    = Todo
+type Model
+    = Model
         { id : String
         , title : String
         , body : String
@@ -15,5 +17,22 @@ type Todo
         }
 
 
-type Store
-    = Store { lookup : Dict String Todo }
+type alias Encoder =
+    Model -> Value
+
+
+encoder : Encoder
+encoder (Model model) =
+    E.object
+        [ ( "id", E.string model.id )
+        , ( "title", E.string model.title )
+        , ( "body", E.string model.body )
+        , ( "done", E.bool model.done )
+        , ( "createdAt", E.int model.createdAt )
+        , ( "modifiedAt", E.int model.modifiedAt )
+        ]
+
+
+
+--type Store
+--    = Store { lookup : Dict String Todo }
