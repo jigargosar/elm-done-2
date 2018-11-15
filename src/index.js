5656cc45a2d85aa795fcc53219ffa25c975c6c25
Jigar Gosar <jigar.gosar@gmail.com>
2018-11-15 15:20:32 +0530

    index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,82 @@
+import 'tachyons'
 import './main.css'
+// noinspection ES6CheckImport
 import { Elm } from './Main.elm'
 import registerServiceWorker from './registerServiceWorker'
+import { curry, forEachObjIndexed, isNil, partial, pathOr, tap } from 'ramda'
 
-Elm.Main.init({
-  node: document.getElementById('root')
-});
+export const tapLog = m => tap(partial(console.log, [m]))
 
-registerServiceWorker();
+const sendTo = curry(function sendTo(app, port, data) {
+  if (!pathOr(null, ['ports', port, 'send'])(app)) {
+    console.error('sendTo port not found', port, 'data ignored', data)
+    return
+  }
+  app.ports[port].send(data)
+})
+
+function subscribe(options, app) {
+  if (!app || !app.ports) {
+    console.error('no ports found', app)
+    return
+  }
+
+  forEachObjIndexed((fn, sub) => {
+    if (!pathOr(null, ['ports', sub, 'subscribe'])(app)) {
+      console.error('sub port not found', sub)
+      return
+    }
+    // noinspection JSIgnoredPromiseFromCall, JSCheckFunctionSignatures
+    app.ports[sub].subscribe(data => fn(data, sendTo(app)))
+  })(options)
+}
+
+function storageGetOr(defaultValue, key) {
+  try {
+    let item = localStorage.getItem(key)
+    if (isNil(item)) return defaultValue
+    return JSON.parse(item)
+  } catch (e) {
+    return defaultValue
+  }
+}
+
+function storageSet(key, value) {
+  if (isNil(value) || isNil(key)) {
+    console.warn('Invalid Args', 'storageSet', key, value)
+    return
+  }
+  localStorage.setItem(key, JSON.stringify(value))
+}
+
+let initialTodos = storageGetOr({}, 'todos')
+let initialContexts = storageGetOr({}, 'contexts')
+// console.log("initialTodos",initialTodos)
+
+// noinspection JSUnresolvedVariable
+const app = Elm.Main.init({
+  node: document.getElementById('root'),
+  flags: {
+    now: Date.now(),
+    windowSize: { width: window.innerWidth, height: window.innerHeight },
+    todos: initialTodos,
+    contexts: initialContexts,
+  },
+})
+
+subscribe(
+  {
+    warn: data => {
+      console.warn(data)
+    },
+    cacheTodoStore: todos => {
+      storageSet('todos', todos)
+    },
+    cacheContextStore: contexts => {
+      storageSet('contexts', contexts)
+    },
+  },
+  app,
+)
+
+registerServiceWorker()
