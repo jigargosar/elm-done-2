7b2f5ac0b74c2df783b3180b7df6d882dbcaf549
Jigar Gosar <jigar.gosar@gmail.com>
2018-11-16 14:29:48 +0530

    Hotkey mappings

--- src/Main.elm
+++ src/Main.elm
@@ -80,7 +80,7 @@ viewContent model =
 viewInput model =
     el [ p4 ]
         (Input.text
-            [ Border.rounded u2, onKeyDown [ HotKey.mapEnter Submit ] ]
+            [ Border.rounded u2, onKeyDown <| HotKey.keyMap [ HotKey.mapEnter Submit ] ]
             { onChange = InputChanged
             , text = model.inputText
             , placeholder = Just <| Input.placeholder [] (t "Title...")
