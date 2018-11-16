6791f3c1a2514c2eab24c4d4ce5c2dcdf45b91ea
Jigar Gosar <jigar.gosar@gmail.com>
2018-11-16 17:12:07 +0530

    hotkey helpers

--- src/HotKey.elm
+++ src/HotKey.elm
@@ -1,6 +1,7 @@
 module HotKey exposing (Event, SoftKey(..), bindAll, bindEnter, bindEsc, decoder, enter, esc, initEvent, mapDecoder, onKeyDown, singletonBool)
 
 import BasicsX exposing (..)
+import Html
 import Html.Events
 import Json.Decode as D exposing (Decoder)
 import Json.Encode as E
@@ -14,6 +15,10 @@ type SoftKey
 
 
 type alias Event =
+    ( List SoftKey, String )
+
+
+type alias HotKey =
     ( List SoftKey, String )
 
 
@@ -58,29 +63,40 @@ bindAll mappings =
             )
 
 
+enter : HotKey
 enter =
     ( [], "Enter" )
 
 
+esc : HotKey
 esc =
     ( [], "Escape" )
 
 
+bind : HotKey -> msg -> Decoder msg
 bind hotkey tag =
     bindAll [ ( hotkey, tag ) ]
 
 
+bindEnter : msg -> Decoder msg
 bindEnter =
     bind enter
 
 
+bindEsc : msg -> Decoder msg
 bindEsc =
     bind esc
 
 
+onKeyDown : (Event -> msg) -> Html.Attribute msg
 onKeyDown handler =
     Html.Events.on "keydown"
         (D.map
             handler
             decoder
         )
+
+
+onEsc : msg -> Html.Attribute msg
+onEsc =
+    Html.Events.on "keydown" << bindEnter
