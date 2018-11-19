1900c45125217b0e5b937f6139bcbb5a484721dc
Jigar Gosar <jigar.gosar@gmail.com>
2018-11-19 14:45:01 +0530

    list selection

--- src/TodoLI.elm
+++ src/TodoLI.elm
@@ -1,4 +1,16 @@
-module TodoLI exposing (Msg(..), displayTitle, doneCheckBox, selectionIndicator, view, xSelectionIndicator)
+module TodoLI exposing
+    ( FuzzyTodo
+    , Item(..)
+    , Msg(..)
+    , TodoList
+    , displayTitle
+    , doneCheckBox
+    , getFocusSelectorFor
+    , initList
+    , selectionIndicator
+    , view
+    , xSelectionIndicator
+    )
 
 import BasicsX exposing (..)
 import El exposing (..)
@@ -7,11 +19,105 @@ import Element.Border as Border
 import Element.Events as EE exposing (onClick)
 import Element.Input as EI
 import EventX
+import Fuzzy
 import HotKey
 import Html.Attributes exposing (class)
 import Icons
+import List as L
+import List.Extra as L
 import MaterialColorsUI exposing (..)
-import Todo
+import Maybe as M
+import Maybe.Extra as M
+import SelectionList exposing (Selection, SelectionList)
+import Todo exposing (Todo, TodoStore)
+
+
+type alias FuzzyValue a =
+    { value : a, result : Fuzzy.Result }
+
+
+initFuzzyValue : a -> Fuzzy.Result -> FuzzyValue a
+initFuzzyValue a result =
+    { value = a, result = result }
+
+
+getScore : FuzzyValue a -> Int
+getScore =
+    .result >> .score
+
+
+isScoreGreaterThan num { result } =
+    result.score > num
+
+
+isScoreLessThan num { result } =
+    result.score < num
+
+
+type alias FuzzyTodo =
+    FuzzyValue Todo
+
+
+type Item
+    = FuzzyTodoLI FuzzyTodo
+    | CreateTodoLI String
+
+
+
+--    | InputField String
+
+
+toFuzzyTodoList : String -> List Todo -> List FuzzyTodo
+toFuzzyTodoList query =
+    let
+        boil =
+            String.toLower
+
+        filterFn todo =
+            Fuzzy.match [] [] (boil query) (boil <| todo.title)
+                |> initFuzzyValue todo
+    in
+    L.map filterFn
+
+
+type alias TodoList =
+    SelectionList Item
+
+
+initList :
+    { query : String, todoStore : TodoStore, selection : Selection }
+    -> SelectionList Item
+initList { query, todoStore, selection } =
+    let
+        fuzzyTodoList =
+            Todo.all todoStore
+                |> toFuzzyTodoList query
+
+        items =
+            if isBlank query then
+                fuzzyTodoList |> L.map FuzzyTodoLI
+
+            else
+                fuzzyTodoList
+                    |> L.filter (isScoreLessThan 1000)
+                    |> L.sortBy getScore
+                    |> L.map FuzzyTodoLI
+                    |> (::) (CreateTodoLI query)
+    in
+    selection |> SelectionList.withList items
+
+
+itemDomId suffix =
+    "todo-li--" ++ suffix
+
+
+getFocusSelectorFor item =
+    case item of
+        FuzzyTodoLI { value } ->
+            "#" ++ itemDomId value.id ++ " ." ++ xSelectionIndicator
+
+        CreateTodoLI title ->
+            "#" ++ itemDomId "create-todo-action" ++ xSelectionIndicator
 
 
 type Msg
